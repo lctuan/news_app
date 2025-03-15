b9182d741ce32f715246437269b6eba912de9f71
lctuan <ctuan1406@gmail.com>
2025-03-15 15:37:35 +0700

    Update

--- news_app/lib/pages/article_view.dart
+++ news_app/lib/pages/article_view.dart
@@ -2,14 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
-  String blogUrl;
-  ArticleView({required this.blogUrl});
+  final String blogUrl;
+  const ArticleView({required this.blogUrl, Key? key}) : super(key: key);
 
   @override
   State<ArticleView> createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.blogUrl));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,12 +48,57 @@ class _ArticleViewState extends State<ArticleView> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(
-        child: WebView(
-          initialUrl: widget.blogUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+
+// class ArticleView extends StatefulWidget {
+//   String blogUrl;
+//   ArticleView({required this.blogUrl});
+
+//   @override
+//   State<ArticleView> createState() => _ArticleViewState();
+// }
+
+// class _ArticleViewState extends State<ArticleView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               "Buzz",
+//               style: TextStyle(
+//                 color: Color(0xff3280ef),
+//                 fontSize: 30.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             Text(
+//               "Line",
+//               style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 30.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           ],
+//         ),
+//         centerTitle: true,
+//         elevation: 0.0,
+//       ),
+//       body: Container(
+//         child: WebView(
+//           initialUrl: widget.blogUrl,
+//           javascriptMode: JavascriptMode.unrestricted,
+//         ),
+//       ),
+//     );
+//   }
+// }
